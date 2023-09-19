2a6b0f84a2b86078ca397916ae368b175a446477
Ziqi Fang <zfang1207@gmail.com>
2023-09-19 00:18:46 -0700

    clean testing folder

--- app/getSessionTester/page.js
+++
@@ -1,6 +0,0 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-export default async function SessionTester() {
-  const data = await getServerSession(authOptions);
-  return <p>{JSON.stringify(data)}</p>;
-}
